6d2d9a3b44b9cb5852a705a238304c6483f921cb
Jovan Milosevic <jovanm.nis555@gmail.com>
2025-12-07 20:15:47 +0100

    Clean history - remove large files

--- lab1/lab1View.cpp
+++ lab1/lab1View.cpp
@@ -4,6 +4,7 @@
 
 #include "pch.h"
 #include "framework.h"
+#include <cmath>
 // SHARED_HANDLERS can be defined in an ATL project implementing preview, thumbnail
 // and search filter handlers and allows sharing of document code with that project.
 #ifndef SHARED_HANDLERS
@@ -51,16 +52,37 @@ BOOL Clab1View::PreCreateWindow(CREATESTRUCT& cs)
 	return CView::PreCreateWindow(cs);
 }
 
+void Clab1View::DrawRegularPolygon(CDC* pDC, int cx, int cy, int r, int n, float rotAngle)
+{
+	const double PI = 3.14159265358979323846;
+
+	CPoint* temena = new CPoint[n];
+
+	for (int i = 0; i < n; i++)
+	{
+		double ugaoStepeni = rotAngle + i * (360.0 / n);
+
+		double ugaoRadijani = ugaoStepeni * PI / 180.0;
+
+		temena[i].x = cx + (int)(r * cos(ugaoRadijani));
+		temena[i].y = cy + (int)(r * sin(ugaoRadijani));
+	}
+
+	pDC->Polygon(temena, n);
+
+	delete[] temena;
+}
+
 // Clab1View drawing
 
 void Clab1View::OnDraw(CDC* pDC)
 {
 	Clab1Doc* pDoc = GetDocument();
-	ASSERT_VALID(pDoc); 
+	ASSERT_VALID(pDoc);
 	if (!pDoc)
 		return;
 
-	CRect ekran(0,0,500,500);
+	CRect ekran(0, 0, 500, 500);
 	//CRect ekran;
 	//GetClientRect(&ekran);
 	CBrush backgroundBrush(RGB(180, 180, 180));
@@ -74,6 +96,7 @@ void Clab1View::OnDraw(CDC* pDC)
 	CBrush* orangeBrush = new CBrush(RGB(255, 165, 0));
 	CBrush* oldBrush = pDC->SelectObject(orangeBrush);
 
+
 	CBrush* mainOldBrush = oldBrush;
 
 	//NARANDZASTI POLIGON
@@ -86,6 +109,7 @@ void Clab1View::OnDraw(CDC* pDC)
 	pDC->Polygon(orangeDots, 4);
 
 
+
 	//BELI TROUGAO
 	CBrush* whiteBrush = new CBrush(HS_BDIAGONAL, RGB(0, 0, 255));
 	oldBrush = pDC->SelectObject(whiteBrush);
@@ -95,10 +119,19 @@ void Clab1View::OnDraw(CDC* pDC)
 	whiteDots[2] = CPoint(7 * dim, 10 * dim);
 	//pDC->SetBkMode(TRANSPARENT);
 
+	int cx = (4 + 7 + 7) / 3;
+	int cy = (13 + 16 + 10) / 3;
+
 	pDC->Polygon(whiteDots, 3);
+
+	CPen* greenPen1 = new CPen(PS_SOLID, 2, RGB(0, 255, 0));
+	pDC->SelectObject(greenPen1);
+	pDC->SelectObject(GetStockObject(NULL_BRUSH));
+	DrawRegularPolygon(pDC, cx * dim, cy * dim, 17, 8, 0);
 
 
 	//CRVENI TROUGAO
+	pDC->SelectObject(greenPen);
 	CBrush* redBrush = new CBrush(RGB(255, 0, 0));
 	oldBrush = pDC->SelectObject(redBrush);
 	CPoint redDots[3];
@@ -106,7 +139,14 @@ void Clab1View::OnDraw(CDC* pDC)
 	redDots[1] = CPoint(7 * dim, 10 * dim);
 	redDots[2] = CPoint(13 * dim, 10 * dim);
 
+	cx = (7 + 7 + 13) / 3;
+	cy = (16 + 10 + 10) / 3;
+
 	pDC->Polygon(redDots, 3);
+
+	pDC->SelectObject(greenPen1);
+	pDC->SelectObject(GetStockObject(NULL_BRUSH));
+	DrawRegularPolygon(pDC, cx * dim, cy * dim, dim, 7, 90);
 
 
 	//LJUBICASTI POLIGON
@@ -122,6 +162,7 @@ void Clab1View::OnDraw(CDC* pDC)
 
 
 	//ZUTI TROUGAO
+	pDC->SelectObject(greenPen);
 	CBrush* yellowBrush = new CBrush(RGB(255, 255, 0));
 	oldBrush = pDC->SelectObject(yellowBrush);
 	CPoint yellowDots[3];
@@ -129,10 +170,18 @@ void Clab1View::OnDraw(CDC* pDC)
 	yellowDots[1] = CPoint(16 * dim, 7 * dim);
 	yellowDots[2] = CPoint(13 * dim, 4 * dim);
 
+	cx = (10 + 16 + 13) / 3;
+	cy = (7 + 7 + 4) / 3;
+
 	pDC->Polygon(yellowDots, 3);
+
+	pDC->SelectObject(greenPen1);
+	pDC->SelectObject(GetStockObject(NULL_BRUSH));
+	DrawRegularPolygon(pDC, cx * dim, cy * dim, 16, 5, 72);
 
 
 	//ROZI TROUGAO
+	pDC->SelectObject(greenPen);
 	int centarX = (16 * dim + 13 * dim) / 2;
 	int centarY = (7 * dim + 4 * dim) / 2;
 
@@ -143,10 +192,18 @@ void Clab1View::OnDraw(CDC* pDC)
 	pinkDots[1] = CPoint(20 * dim, 0);
 	pinkDots[2] = CPoint(20 * dim, 11 * dim);
 
+	cx = (pinkDots[0].x + pinkDots[1].x + pinkDots[2].x) / 3;
+	cy = (pinkDots[0].y + pinkDots[1].y + pinkDots[2].y) / 3;
+
 	pDC->Polygon(pinkDots, 3);
+
+	pDC->SelectObject(greenPen1);
+	pDC->SelectObject(GetStockObject(NULL_BRUSH));
+	DrawRegularPolygon(pDC, cx, cy, dim, 4, 0);
 
 
 	//ZELENI TROUGAO
+	pDC->SelectObject(greenPen);
 	CBrush* greenBrush = new CBrush(RGB(0, 200, 0));
 	oldBrush = pDC->SelectObject(greenBrush);
 	CPoint greenDots[3];
@@ -155,6 +212,13 @@ void Clab1View::OnDraw(CDC* pDC)
 	greenDots[2] = CPoint(9 * dim, 0);
 
 	pDC->Polygon(greenDots, 3);
+
+	cx = (greenDots[0].x + greenDots[1].x + greenDots[2].x) / 3;
+	cy = (greenDots[0].y + greenDots[1].y + greenDots[2].y) / 3;
+
+	pDC->SelectObject(greenPen1);
+	pDC->SelectObject(GetStockObject(NULL_BRUSH));
+	DrawRegularPolygon(pDC, cx, cy, dim, 6, 0);
 
 
 	//GRID
